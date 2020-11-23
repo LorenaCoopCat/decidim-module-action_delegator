531ffa279c4c1cc3d747127c854d7826b229f39c
Pau Perez <saulopefa@gmail.com>
2020-11-23 16:43:15 +0100

    Upgrade to decidim@0.23

--- lib/decidim/action_delegator/version.rb
+++ lib/decidim/action_delegator/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-action_delegator version.
   module ActionDelegator
-    DECIDIM_VERSION = "0.22.0"
-    VERSION = "0.1"
+    DECIDIM_VERSION = "0.23.0"
+    VERSION = "0.2"
   end
 end
